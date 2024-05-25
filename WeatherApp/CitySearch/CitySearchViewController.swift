f25058b1137ef04e288b2b568bdd8f1d98ebe5a4
Viktor <viktor.prikolota@exinity.com>
2024-05-25 10:38:56 -0300

    Added tableView to CitySearchViewController

--- WeatherApp/CitySearch/CitySearchViewController.swift
+++ WeatherApp/CitySearch/CitySearchViewController.swift
@@ -10,14 +10,46 @@ import SnapKit
 
 final class CitySearchViewController: BaseViewController {
     // MARK: Properties
-    
+    private let tableView = UITableView()
+
+    private let cityList = [
+        "Moscow",
+        "Paris",
+        "Lonon",
+        "Buenos Aires",
+        "Tokio",
+        "Beijing",
+        "Delhi",
+        "Berlin",
+        "Oslo",
+        "Minsk",
+        "Bangkok"
+    ] // MOCK data
+
+    private var filteredCityList: [String] = []
+    private var searchQuery = ""
+
     // MARK: Lifecycle
     override func setup() {
         super.setup()
 
+        setupTableView()
     }
 
     // MARK: Setup UI
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.backgroundColor = .clear
+        tableView.contentInset.left = 16
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
 
     // MARK: Public methods
 }
@@ -25,10 +57,42 @@ final class CitySearchViewController: BaseViewController {
 // MARK: - UISearchResultsUpdating
 extension CitySearchViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.searchTextField.text else { return }
+        guard let query = searchController.searchBar.searchTextField.text else { return }
+        self.searchQuery = query.lowercased()
 
-        view.backgroundColor = .black.withAlphaComponent(text.isEmpty ? 0.5 : 1)
+        view.backgroundColor = .black.withAlphaComponent(searchQuery.isEmpty ? 0.5 : 1)
 
-        print(text)
+        filteredCityList = cityList.filter { $0.lowercased().contains(searchQuery) }
+        tableView.reloadData()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension CitySearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        filteredCityList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let attributedText = NSMutableAttributedString(
+            string: filteredCityList[indexPath.row],
+            attributes: [.foregroundColor: UIColor.white.withAlphaComponent(0.5)]
+        )
+
+        let queryRange = (filteredCityList[indexPath.row].lowercased() as NSString).range(of: searchQuery)
+        attributedText.addAttributes([.foregroundColor: UIColor.white], range: queryRange)
+
+        cell.backgroundColor = .clear
+        cell.textLabel?.attributedText = attributedText
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension CitySearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
     }
 }
